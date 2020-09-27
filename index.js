164ceccbdac764a5370779f65f904770f549c0b9
Ilona-Petenko <71134645+Ilona-Petenko@users.noreply.github.com>
2020-09-27 12:01:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 document.getElementById("show-heart-button").onclick=function() { 
     document.getElementById("heart-img").style.visibility="visible"
     document.getElementById("back-button").style.visibility="visible"
-    document.getElementById("show-heart-button").style.visibility="hidden"
+    document.getElementById("show-heart-button").hidden=true
 } 
 
 document.getElementById("back-button").onclick=function() { 
     document.getElementById("heart-img").style.visibility="hidden"
     document.getElementById("back-button").style.visibility="hidden"
-    document.getElementById("show-heart-button").style.visibility="visible"
+    document.getElementById("show-heart-button").hidden=false
 
 }
